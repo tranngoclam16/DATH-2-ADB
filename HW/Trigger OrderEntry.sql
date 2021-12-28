ccf53b0489f28f3aabcb211bdc6203934546b6e2
ngphvy01 <ngphvy01@users.noreply.github.com>
2021-12-28 16:15:37 +0700

    trigger

---
+++ HW/Trigger OrderEntry.sql
@@ -0,0 +1,32 @@
+﻿use Order_Entry
+GO
+
+create trigger PreferredCard_insert on Credit_Card
+for insert,update
+as
+begin
+	update Customer 
+	set PreferredCreditCard= i.CustomerCreditCardNumber
+	from INSERTED i
+	where i.PreferredOption='Yes'
+	and i.CustomerIdentifier= Customer.CustomerIdentifier
+
+	update Customer 
+	set PreferredCreditCard= NULL
+	from INSERTED i
+	where i.PreferredOption='No'
+	and i.CustomerIdentifier= Customer.CustomerIdentifier
+end
+go
+
+create trigger NumberUsed_Card on Orders
+for insert
+as
+begin
+	update Credit_Card
+	set NumberUsed= NumberUsed+1
+	from INSERTED i
+	where i.CustomerCreditCardNumber=Credit_Card.CustomerCreditCardNumber
+end
+go
+
