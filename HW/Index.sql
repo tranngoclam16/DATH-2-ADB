d2e1028786f4daf3a1d93ad01ca7d11ccfb6bcf3
tktruc211 <tktruc19@clc.fitus.edu.vn>
2021-12-30 06:16:24 +0700

    Update Index (OrderEntry)
    8 câu truy vấn

--- HW/Index.sql
+++ HW/Index.sql
@@ -1,13 +1,33 @@
 ﻿--INDEX
---TRUY VẤN 3
+
+-----TRUY VẤN 3
 --SELECT Customer.*,Credit_Card.CustomerCreditCardNumber,Credit_Card.CustomerCreditCardName ,Credit_Card.NumberUsed							
 --from Customer join Credit_Card on Customer.CustomerIdentifier = Credit_Card.CustomerIdentifier							
 --where Customer.CustomerIdentifier = '35874'
+-----TRUY VẤN 5
+--select [Customer].*, [Credit_Card].CustomerCreditCardNumber, [Credit_Card].NumberUsed							
+--from [Customer] join [Credit_Card] on [Customer].CustomerIdentifer = [Credit_Card].CustomerIdentifer							
+--where [Customer].CustomerIdentifier = @CustomerID							
 CREATE NONCLUSTERED INDEX idx_credit_card_customer_id
 ON [dbo].[Credit_Card] ([CustomerIdentifier])
 GO
 
---TRUY VẤN 7
+-----TRUY VẤN 1
+--select * from [Order]				
+--where [Order].CustomerIdentifer = @CustomerID				
+-----TRUY VẤN 6
+--SELECT C.*, NumberOfOrders					
+--FROM Customer C					
+--INNER JOIN					
+--(SELECT COUNT(O.OrderNumber) as NumberOfOrders, CustomerIdentifier					
+--FROM Orders O					
+--GROUP BY O.CustomerIdentifier) t2					
+--ON C.CustomerIdentifier = t2.CustomerIdentifier		
+CREATE NONCLUSTERED INDEX IX_Order_CustomerIdentifier
+on [dbo].[Orders] ([CustomerIdentifier])
+GO
+
+-----TRUY VẤN 7
 --SELECT TOP 20 * FROM Advertised_Item ai							
 --JOIN (SELECT ItemNumber, SUM(QuantityOrdered) as NumbersOfItem							
 --FROM Ordered_Item							
@@ -17,7 +37,7 @@ CREATE NONCLUSTERED INDEX IX_OrderedItem_QuantityOrdered
 on [dbo].[Ordered_Item] ([QuantityOrdered])
 GO
 
---TRUY VẤN 8
+-----TRUY VẤN 8
 --Select * from Supplier where Supplier.Total_Item= ( Select MAX (Total_Item) from Supplier)
 CREATE NONCLUSTERED INDEX idx_total_item_supplier
 ON [dbo].[Supplier] ([Total_Item])
