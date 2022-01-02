0eccd7a0a0946900793f5743524999e411ea5c97
ngphvy01 <ngphvy01@users.noreply.github.com>
2022-01-02 10:04:32 +0700

    update procedure list

--- ProcedureList.sql
+++ ProcedureList.sql
@@ -1,16 +1,6 @@
 ﻿use DATH#2
 GO
 
-/*
-Danh sách các procedure đã tồn tại:
-
-	Tạo tài khoản khách hàng: sp_CreateAccount_KH
-	Xem lịch sử tích lũy: getLSTL
-	Xem lịch sử mua hàng: xem_LichSuMuaHang_KH
-
-*/
-
-
 --Tạo tài khoản khách hàng
 CREATE PROC sp_CreateAccount_KH
 	@HoTen nvarchar(100),
@@ -24,7 +14,7 @@ AS
 BEGIN
 	BEGIN TRANSACTION
 		BEGIN TRY
-			INSERT INTO KhachHang VALUES (@HoTen,@SDT,1,@GioiTinh,@NgaySinh,@DiaChi,@Email,@pword); 
+			INSERT INTO KhachHang VALUES (@HoTen,@SDT,1,@GioiTinh,@NgaySinh,@DiaChi,@Email,@pword,0); 
 		COMMIT TRANSACTION
 		END TRY
 		BEGIN CATCH
@@ -37,29 +27,6 @@ BEGIN
 END;
 GO
 --DROP PROC sp_CreateAccount_KH
---------------------------------------------------------------
---Xem lịch sử mua hàng
-CREATE PROCEDURE xem_LichSuMuaHang_KH
-	@sdtKH varchar(10)
-as
-BEGIN TRAN
-	if not EXISTS
-	(
-		SELECT * from KhachHang
-		WHERE KhachHang.SDT = @sdtKH
-	)
-	BEGIN
-		Raiserror('Không tồn tại Số điện thoại khách hàng này.', 16, 1)
-		ROLLBACK TRAN
-	END
-
-	else
-	BEGIN
-		SELECT * from DonHang
-		WHERE DonHang.MaKH = @sdtKH
-	END
-COMMIT TRAN
-GO
 -----------------------------------------------------------------------
 --Xem lịch sử tích lũy
 create procedure getLSTL
